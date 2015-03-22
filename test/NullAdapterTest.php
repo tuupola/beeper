d64c9b856c427a2b5cb56b70202da11adaef810b
Mika Tuupola <tuupola@appelsiini.net>
2015-03-23 00:53:44 +0700

    Update copyright year

--- test/NullAdapterTest.php
+++ test/NullAdapterTest.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Beeper package
  *
- * Copyright (c) 2014 Mika Tuupola
+ * Copyright (c) 2014-2015 Mika Tuupola
  *
  * Licensed under the MIT license:
  *   http://www.opensource.org/licenses/mit-license.php
