ee726b799e65bf332f8945a927de94d3e6538632
Mika Tuupola <tuupola@appelsiini.net>
2017-01-09 16:56:56 +0700

    Hello 2017

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Beeper package
  *
- * Copyright (c) 2014-2016 Mika Tuupola
+ * Copyright (c) 2014-2017 Mika Tuupola
  *
  * Licensed under the MIT license:
  *   http://www.opensource.org/licenses/mit-license.php
